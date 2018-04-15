5b68f673c1a03746c7059fb3fb952fc3963ba423
Emanuele Formento <>
2018-04-15 03:10:33 +0200

    added myo controller v1

--- python/EmgController.py
+++ python/EmgController.py
@@ -2,6 +2,8 @@ import numpy as np
 import time
 import logging
 from Queue import Queue, Empty
+from myo import MyoRaw
+import threading
 
 logging.basicConfig(level=logging.DEBUG,format="(%(threadName)-9s) %(message)s",)
 
@@ -13,9 +15,18 @@ class EmgController():
         self._slidingWindowSamples = int(slidingWindowSamples)
         self._stopControl = False
         self._decoder = emgDecoder
+        self._myo = MyoRaw(None)
+        self._myo.connect()
+
+        self._threadMyoReading = threading.Thread(name="myoThread",target=self.get_myo_data_window)
+        self._threadMyoReading.start()
+
+        # return np.random.rand(6,self._slidingWindowSamples)
 
     def get_myo_data_window(self):
-        return np.random.rand(6,self._slidingWindowSamples)
+        #TODO this is shit
+        while not self._stopControl:
+            self._myo.run(timeout=1)
 
     def get_myo_data(self,nSeconds):
         return np.random.rand(6,nSeconds)
@@ -26,7 +37,9 @@ class EmgController():
         while not self._stopControl:
             if time.time()-oldTime>= self._outputPeriod:
                 oldTime = time.time()
-                emgWindow = self.get_myo_data_window()
+
+                emgWindow = self._myo.myo_emg_buffer
+
                 velocity = self._decoder.decode(emgWindow)
                 queue.put(velocity)
                 logging.debug("Decoded emg data!")
--- python/MotorController.py
+++ python/MotorController.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import logging
 from Queue import Queue, Empty
+import RPi.GPIO as GPIO
 
 logging.basicConfig(level=logging.DEBUG,format="(%(threadName)-9s) %(message)s",)
 
@@ -13,8 +14,25 @@ class MotorController():
         self._stopControl = False
         self._velocity = 0
 
+        self._outputRpiPin = 18
+        self._pwmOptimalFreq = 50
+        self._motorPosition = 5.5 # half range of motion
+
     def _update_motor_position(self):
-        pass
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self._outputRpiPin, GPIO.OUT)
+
+        pwm = GPIO.PWM(self._outputRpiPin,self._pwmOptimalFreq)
+        pwm.start(0)
+
+        GPIO.output(self._outputRpiPin,True)
+        pwm.ChangeDutyCycle(self._motorPosition)
+        time.sleep(0.5)
+        self._motorPosition += self._velocity
+
+        pwm.stop()
+        GPIO.cleanup()
+
 
     def run_motor(self,queue):
         oldTime = -100
@@ -24,7 +42,20 @@ class MotorController():
                 oldTime = time.time()
                 try: self._velocity = queue.get()
                 except Empty: pass
-                self._update_motor_position()
+
+                # Motor control
+                # GPIO.setmode(GPIO.BOARD)
+                # GPIO.setup(self._outputRpiPin, GPIO.OUT)
+                # pwm = GPIO.PWM(self._outputRpiPin,self._pwmOptimalFreq)
+                # pwm.start(0)
+                # GPIO.output(self._outputRpiPin,True)
+                # pwm.ChangeDutyCycle(self._motorPosition)
+
+                # time.sleep(0.5)
+                # self._motorPosition += self._velocity
+                # pwm.stop()
+                # GPIO.cleanup()
+
                 logging.debug("velocity is {0}".format(self._velocity))
 
     def set_stopControl(self,val):
